849002c1e4780c90e06a6a5dbdfc28a856e2bc2c
kigehiflorence <florencekigehi@gmail.com>
2025-06-15 21:45:51 +0300

    Update _app.tsx

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,15 +1,15 @@
 import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import store from "@/store/store";
+import { CountProvider } from "@/context/CountContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+    <CountProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </Provider>
+    </CountProvider>
   );
 }
+
